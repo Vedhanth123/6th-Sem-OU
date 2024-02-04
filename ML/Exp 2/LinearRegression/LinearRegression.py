be025fb2d8c7866885499c1ac60b870d7d5ae9f2
Vedhanth <gvedhanth123@outlook.com>
2024-02-04 14:23:59 +0530

    Machine Learning Experiment 2

---
+++ ML/Exp 2/LinearRegression/LinearRegression.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv")
--- NLP/test.py
+++ NLP/test.py
@@ -1,5 +1,5 @@
 from nltk.book import *
-# from matplotlib.pyplot import *
+from matplotlib.pyplot import *
 
 # print("This is the output-----------------")
 # text2.similar("monstrous")
@@ -10,8 +10,9 @@ from nltk.book import *
 # print("THis is the ouput of commonn_contexts")
 # text2.common_contexts(['monstrous', 'very'])
 
-# print("THis is the ouput of dispersion_plot")
-# text4.dispersion_plot(['citizens','democracy','freedom','duties','america'])
+print("THis is the ouput of dispersion_plot")
+text4.dispersion_plot(['citizens','democracy','freedom','duties','america'])
+show()
 
 print("THis is the ouput of generate")
 text3.generate()
